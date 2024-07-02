8fd60b9d301120f2a75ef2042f902d16f703fa90
candela-greco <candelagreco15@gmail.com>
2024-07-01 22:05:34 -0300

    Genere otros scripts para alquileres y ventas

---
+++ assets/js/alquileres.js
@@ -0,0 +1,68 @@
+const propiedadesAlquiler = [
+    {
+      src: './assets/imgs/alquiler1.avif',
+      titulo: 'Departamento en zona residencial',
+      descripcion: 'Departamento vivienda familiar en zona residencial',
+      ubicacion: 'En zona residencial',
+      habitaciones: '8 habitaciones |',
+      baños: '4 baños',
+      costo: 500000,
+      smoke: false,
+      pets: true
+    },
+    {
+      src: './assets/imgs/alquiler2.avif',
+      titulo: 'Departamento en zona centrica',
+      descripcion: 'Departamento en zona centrica',
+      ubicacion: 'En zona centrica',
+      habitaciones: '8 habitaciones |',
+      baños: '4 baños',
+      costo: 2000000,
+      smoke: true,
+      pets: false
+    },
+    {
+      src: './assets/imgs/alquiler3.avif',
+      titulo: 'Departamento en zona residencial',
+      descripcion: 'Hermoso departamento en zona residencial',
+      ubicacion: 'En zona residencial',
+      habitaciones: '8 habitaciones |',
+      baños: '4 baños',
+      costo: 3000000,
+      smoke: false,
+      pets: true
+    },
+    {
+      src: './assets/imgs/alquiler4.avif',
+      titulo: 'Departamento en los suburbios',
+      descripcion: 'Departamento en zona suburbios',
+      ubicacion: 'En suburbios',
+      habitaciones: '8 habitaciones |',
+      baños: '4 baños',
+      costo: 4000000,
+      smoke: true,
+      pets: false
+    },
+    {
+        src: './assets/imgs/alquiler3.avif',
+        titulo: 'Departamento en zona residencial',
+        descripcion: 'Hermoso departamento en zona residencial',
+        ubicacion: 'En zona residencial',
+        habitaciones: '8 habitaciones |',
+        baños: '4 baños',
+        costo: 3000000,
+        smoke: false,
+        pets: true
+      },
+      {
+        src: './assets/imgs/alquiler4.avif',
+        titulo: 'Departamento en los suburbios',
+        descripcion: 'Departamento en zona suburbios',
+        ubicacion: 'En suburbios',
+        habitaciones: '8 habitaciones |',
+        baños: '4 baños',
+        costo: 4000000,
+        smoke: true,
+        pets: false
+      }
+  ];
