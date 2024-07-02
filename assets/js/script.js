263558f9bb5d045ac09ad45de2e8f07c1994c382
candela-greco <candelagreco15@gmail.com>
2024-07-01 21:23:38 -0300

    subo zona alquileres, botones a paginas y html de estas nuevas paginas

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,3 +90,97 @@ for (let venta of propiedadesVenta) {
 }
 
 ventas.innerHTML = template1;
+
+
+const propiedadesAlquiler = [
+  {
+    src: './assets/imgs/alquiler1.avif',
+    titulo: 'Departamento en zona residencial',
+    descripcion: 'Departamento vivienda familiar en zona residencial',
+    ubicacion: 'En zona residencial',
+    habitaciones: '8 habitaciones |',
+    baños: '4 baños',
+    costo: 500000,
+    smoke: false,
+    pets: true
+  },
+  {
+    src: './assets/imgs/alquiler2.avif',
+    titulo: 'Departamento en zona centrica',
+    descripcion: 'Departamento en zona centrica',
+    ubicacion: 'En zona centrica',
+    habitaciones: '8 habitaciones |',
+    baños: '4 baños',
+    costo: 2000000,
+    smoke: true,
+    pets: false
+  },
+  {
+    src: './assets/imgs/alquiler3.avif',
+    titulo: 'Departamento en zona residencial',
+    descripcion: 'Hermoso departamento en zona residencial',
+    ubicacion: 'En zona residencial',
+    habitaciones: '8 habitaciones |',
+    baños: '4 baños',
+    costo: 3000000,
+    smoke: false,
+    pets: true
+  },
+  {
+    src: './assets/imgs/alquiler4.avif',
+    titulo: 'Departamento en los suburbios',
+    descripcion: 'Departamento en zona suburbios',
+    ubicacion: 'En suburbios',
+    habitaciones: '8 habitaciones |',
+    baños: '4 baños',
+    costo: 4000000,
+    smoke: true,
+    pets: false
+  }
+];
+
+const alquileres = document.querySelector(".alquileres");
+
+let template2 = "";
+
+for (let alquiler of propiedadesAlquiler) {
+
+  let fumar = "";
+  let animales = "";
+
+  if (alquiler.smoke === true) {
+    fumar = `<p style="color: green"><i class="fa-solid fa-smoking"> Permitido fumar</i></p>`;
+  } else {
+    fumar = `<p style="color: red"><i class="fa-solid fa-smoking"> No se permite fumar</i></p>`;
+  }
+
+  if (alquiler.pets === true) {
+    animales = `<p style="color: green"><i class="fa-solid fa-paw"> Mascotas permitidas</i></p>`;
+  } else {
+    animales = `<p style="color: red"><i class="fa-solid fa-paw"> No se permiten mascotas</i></p>`;
+  }
+
+  template2 += `
+    <div class="cardAlquiler">
+      <img class="alquiler-img" src="${alquiler.src}">
+      <div class="alquiler-cuerpo">
+        <h3 class="alquiler-tittle">${alquiler.titulo}</h3>
+        <p>${alquiler.descripcion}</p>
+        <div class="alquiler-ubicacion">
+          <i class="fa-solid fa-location-dot"></i> <p>${alquiler.ubicacion}</p>
+        </div>
+        <div class="alquiler-habitaciones">
+          <i class="fa-solid fa-bed"></i> <p>${alquiler.habitaciones}</p>
+          <i class="fa-solid fa-bath baño"></i> <p>${alquiler.baños}</p>
+        </div>
+        <div class="alquiler-costo">
+          <i class="fa-solid fa-dollar-sign"></i> <p>$${alquiler.costo}</p>
+        </div>
+        ${fumar}
+        ${animales}
+      </div>
+    </div>
+  `;
+}
+
+alquileres.innerHTML = template2;
